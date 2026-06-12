0ba5f6e43d1002ecd7b9d8cecd2d29cdf81ca1d8
Kosorukov Anatolii <java1cprog@gmail.com>
2026-06-12 12:36:22 +0700

    Add Flask routes and templates for home page and posts display

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Flask based project
 
 Run Flask app: 
 - `flask --app microblog run`
+
+Run Flask app: 
+- `flask run`
+
+## Extra info
+- [Miguel's blog](https://blog.miguelgrinberg.com/)
+- [Ru Translation](https://habr.com/ru/articles/804245/)
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,19 @@
 """Routes for the Flask application."""
+
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    """Route for the home page."""
-    return "Hello, World!"
+    """Render the home page."""
+    user = {"username": "Miguel"}
+
+    posts = [
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
+    # return render_template("index.html",  user=user)
+    # return render_template("index.html", title="Home", user=user)
