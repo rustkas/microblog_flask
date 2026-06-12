10d8c9e267a7b2266a6c8456edf71f3053844f75
Kosorukov Anatolii <java1cprog@gmail.com>
2026-06-12 13:27:37 +0700

    Enhance login functionality and templates with error handling and URL routing

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 """Routes for the Flask application."""
 
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -20,7 +20,7 @@ def index():
     # return render_template("index.html", title="Home", user=user)
 
 
-@app.route("/login")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """Render the login page."""
     form = LoginForm()
@@ -28,5 +28,5 @@ def login():
         flash(
             f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
         )
-        return redirect("/index")
+        return redirect(url_for('index'))
     return render_template("login.html", title="Sign In", form=form)
